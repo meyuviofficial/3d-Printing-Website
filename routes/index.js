daa8a6f4ba59f8c38d1c0c57c8360b23e20892ee
Yuvaraj Selvarajan <Yuvaraj.Selvarjan@shell.com>
2020-03-21 01:20:36 +0530

    added router middleware

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,18 @@
 const express = require('express')
-const app = express()
-const path = require('path')
-// const routes = express.Router();
+const routes = express.Router();
 
-        app.get('/', function (req, res) {
+        routes.get('/', function (req, res) {
             res.render('index')
         })
-        app.get('/3D-Printers', function (req, res) {
+        routes.get('/3D-Printers', function (req, res) {
             res.render('Printers')
         })
-        app.get('/3D-Printing-Services', function (req, res) {
+        routes.get('/3D-Printing-Services', function (req, res) {
             res.render('Printing_Services')
         })
-        app.get('/Filaments', function (req, res) {
+        routes.get('/Filaments', function (req, res) {
             res.render('filaments')
         })
         console.log("hey Yuvi !! I'm from indexx.js");
 
-module.exports = app;
+module.exports = routes;
