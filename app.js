153523dca76d8b91fe2a87c3db97867a74ba2cc1
Yuvaraj Selvarajan <Yuvaraj.Selvarjan@shell.com>
2020-03-22 00:44:59 +0530

    added comments

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+//initializing for express
 const express = require('express')
 const app = express()
+//initializing for path
 const path = require('path')
-const routes=require('./routes/index');
+//initializing for mongoDB
 const MongoClient = require('mongodb').MongoClient;
+// including routes config file
+const routes=require('./routes/index');
+//including DB config file
 const dbConfig = require('./model/database')
 
 app.use('/',routes);
+
+//setting the view engine 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use('/static', express.static(path.join(__dirname, 'public')))
+//setting the static folder for js, CSS, img (static files)
+app.use('/static', express.static(path.join(__dirname, 'public')));
 console.log("Hey Yuvi");
+//server listens at 3000
 app.listen(3000)
 
