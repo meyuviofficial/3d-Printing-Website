6285a0f3a3cc0c5f069f1cb13ecb677b212d2f58
Yuvaraj Selvarajan <Yuvaraj.Selvarjan@shell.com>
2020-03-22 00:29:06 +0530

    Mongo DB connection

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const routes=require('./routes/index');
-
-console.log("Hey Suvi");
-app.use('/',routes);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
+const MongoClient = require('mongodb').MongoClient;
+const ConnectionString = "mongodb+srv://Yuvi:fa2SPsJMU8SNIgZI@yuvarajclusterwebsite-8nhjd.azure.mongodb.net/YuvarajSampleDB?retryWrites=true&w=majority"
+var url = "mongodb://localhost:27017/mydb";
+MongoClient.connect(ConnectionString, function (err, db) {
+     if (err) throw err;
+     console.log("Database created!");
+     var dbo = db.db("YuvarajSampleDB");
+     var myobj = {
+         name: "Yuvaraj Inc",
+         address: "Highway 37"
+     };
+     dbo.collection("YuvarajSampleCollection").insertOne(myobj, function (err, res) {
+                 if (err) throw err;
+                 console.log("1 document inserted");
+                 db.close();
+     });
+    });
 console.log("Hey Yuvi");
 app.listen(3000)
 
