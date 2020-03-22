cfc1fcd06c07e3591e6493de99c39dd6ed2ddf3a
Yuvaraj Selvarajan <Yuvaraj.Selvarjan@shell.com>
2020-03-22 14:26:24 +0530

    included passport package

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express') //initializing for express
 const app = express()
 const path = require('path') //initializing for path
 const MongoClient = require('mongodb').MongoClient; //initializing for mongoDB
-const routes = require('./routes/index'); // including routes config file
 const dbConfig = require('./model/database') //including DB config file
 var passport = require('passport');
 var bodyParser = require('body-parser');
@@ -11,9 +10,10 @@ require('./config/passport')(passport);
 
 app.use(passport.initialize());
 app.use(bodyParser());
-app.use('/',routes);
+// app.use('/',routes);
 app.set('view engine', 'ejs'); //setting the view engine 
 app.set('views', path.join(__dirname, 'views'));
 app.use('/static', express.static(path.join(__dirname, 'public'))); //setting the static folder for js, CSS, img (static files)
+const routes = require('./routes/index')(app,passport); // including routes config file
 console.log("Hey Yuvi");
 app.listen(3000) //server listens at 3000
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const routes = express.Router();
+module.exports = function(app,passport)
+{
 
-        routes.get('/', function (req, res) {
-            res.render('index')
-        })
-        routes.get('/3D-Printers', function (req, res) {
-            res.render('Printers')
-        })
-        routes.get('/3D-Printing-Services', function (req, res) {
-            res.render('Printing_Services')
-        })
-        routes.get('/Filaments', function (req, res) {
-            res.render('filaments')
-        })
-        console.log("hey Yuvi !! I'm from indexx.js");
-        routes.get('/login',function(req,res)
-        {
-            res.render('login')
-        });
-        routes.get('/signup',function(req,res)
-        {
-            res.render('signup');
-        });
-        routes.post('/signup',function(req,res)
-        {
-            res.render('index');
-        });
-module.exports = routes;
+    app.get('/', function (req, res) {
+        res.render('index')
+    })
+    app.get('/3D-Printers', function (req, res) {
+        res.render('Printers')
+    })
+    app.get('/3D-Printing-Services', function (req, res) {
+        res.render('Printing_Services')
+    })
+    app.get('/Filaments', function (req, res) {
+        res.render('filaments')
+    })
+    console.log("hey Yuvi !! I'm from indexx.js");
+    app.get('/login',function(req,res)
+    {
+        res.render('login')
+    });
+    app.get('/signup',function(req,res)
+    {
+        res.render('signup');
+    });
+    app.post('/signup',function(req,res)
+    {
+        res.render('index');
+    });
+}
+// module.exports = routes;
